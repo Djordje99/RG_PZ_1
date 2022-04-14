c589a0bffcbfd17fa971126a2447f0e6faf8c9fe
Djordje99 <bozovicdjordje4@gmail.com>
2022-04-14 16:29:26 +0200

    BFS implemented bug with lines

--- PredmetniZadatak_1/PredmetniZadatak_1/BFS/BfsAglorithm.cs
+++ PredmetniZadatak_1/PredmetniZadatak_1/BFS/BfsAglorithm.cs
@@ -6,98 +6,158 @@ using System.Threading.Tasks;
 
 namespace PredmetniZadatak_1.BFS
 {
-    // This class represents a directed
-    // graph using adjacency list
-    // representation
-    class BfsAlgorithm
+    public struct Node
     {
+        public int row;
+        public int colum;
+    }
 
-        // No. of vertices    
-        private int _V;
+    public class BfsAlgorithm
+    {
+        private int rowSize = 960;
+        private int columSize = 960;
+        private int[,] matrix;
+        private Queue<int> rowQueue = new Queue<int>();
+        private Queue<int> columQueue = new Queue<int>();
+        private int moveCount = 0;
+        private int nodesLeftInLayer = 1;
+        private int nodesInNextLayer = 0;
+        private bool isEnd = false;
+        private bool[,] visitedNodes;
+        private int[] directionRow = new int[] { -1, 1, 0, 0 };
+        private int[] directionColum = new int[] { 0, 0, 1, -1 };
+        private Node[,] prev;
 
-        //Adjacency Lists
-        LinkedList<int>[] _adj;
-
-        public BfsAlgorithm(int V)
+        public BfsAlgorithm()
         {
-            _adj = new LinkedList<int>[V];
-            for (int i = 0; i < _adj.Length; i++)
+            matrix = new int[rowSize, columSize];
+            visitedNodes = new bool[rowSize, columSize];
+            prev = new Node[rowSize, columSize];
+
+            for (int i = 0; i < rowSize; i++)
             {
-                _adj[i] = new LinkedList<int>();
-            }
-            _V = V;
-        }
-
-        // Function to add an edge into the graph
-        public void AddEdge(int v, int w)
-        {
-            _adj[v].AddLast(w);
-
-        }
-
-        // Prints BFS traversal from a given source s
-        public void BFS(int s)
-        {
-
-            // Mark all the vertices as not
-            // visited(By default set as false)
-            bool[] visited = new bool[_V];
-            for (int i = 0; i < _V; i++)
-                visited[i] = false;
-
-            // Create a queue for BFS
-            LinkedList<int> queue = new LinkedList<int>();
-
-            // Mark the current node as
-            // visited and enqueue it
-            visited[s] = true;
-            queue.AddLast(s);
-
-            while (queue.Any())
-            {
-
-                // Dequeue a vertex from queue
-                // and print it
-                s = queue.First();
-                Console.Write(s + " ");
-                queue.RemoveFirst();
-
-                // Get all adjacent vertices of the
-                // dequeued vertex s. If a adjacent
-                // has not been visited, then mark it
-                // visited and enqueue it
-                LinkedList<int> list = _adj[s];
-
-                foreach (var val in list)
+                for (int j = 0; j < columSize; j++)
                 {
-                    if (!visited[val])
-                    {
-                        visited[val] = true;
-                        queue.AddLast(val);
-                    }
+                    matrix[i, j] = 0;
                 }
             }
         }
+
+        public int Solve(int startRow, int startColum, int endRow, int endColum)
+        {
+            InitStart();
+
+            int currentRow, currentColum;
+
+            rowQueue.Enqueue(startRow);
+            columQueue.Enqueue(startColum);
+            visitedNodes[startRow, startColum] = true;
+
+            while(rowQueue.Count > 0)
+            {
+                currentRow = rowQueue.Dequeue();
+                currentColum = columQueue.Dequeue();
+
+                if(currentRow == endRow && currentColum == endColum)
+                {
+                    isEnd = true;
+                    break;
+                }
+
+                ExploreNodes(currentRow, currentColum);
+
+                nodesLeftInLayer--;
+
+                if(nodesLeftInLayer == 0)
+                {
+                    nodesLeftInLayer = nodesInNextLayer;
+                    nodesInNextLayer = 0;
+                    moveCount++;
+                }
+            }
+
+            if (isEnd)
+                return moveCount;
+
+            return -1;
+        }
+
+        private void ExploreNodes(int currentRow, int currentColum)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int nextRow = currentRow + directionRow[i];
+                int nextColum = currentColum + directionColum[i];
+
+                if (nextRow < 0 || nextColum < 0)
+                    continue;
+                if (nextRow >= rowSize || nextColum >= columSize)
+                    continue;
+                if (visitedNodes[nextRow, nextColum])
+                    continue;
+                if (matrix[nextRow, nextColum] == 1)
+                    continue;
+
+                rowQueue.Enqueue(nextRow);
+                columQueue.Enqueue(nextColum);
+
+                visitedNodes[nextRow, nextColum] = true;
+                nodesInNextLayer++;
+
+                Node node = new Node();
+                node.row = currentRow;
+                node.colum = currentColum;
+
+                prev[nextRow, nextColum] = node;
+            }
+        }
+
+        public List<Node> ReconstructPath(int startRow, int startColum, int endRow, int endColum)
+        {
+            List<Node> path = new List<Node>();
+
+            int rowRead = endRow;
+            int columRead = endColum;
+
+            for (int i = 0; i < moveCount + 1; i++)
+            {
+                Node node = new Node();
+                node.row = rowRead;
+                node.colum = columRead;
+
+                path.Add(node);
+
+                Node n = prev[rowRead, columRead];
+
+                rowRead = n.row;
+                columRead = n.colum;
+            }
+
+            path.Reverse();
+
+            if(path[0].row == startRow && path[0].colum == startColum)
+            {
+                foreach (var item in path)
+                {
+                    matrix[item.row, item.colum] = 1;
+                }
+                return path;
+            }
+
+            path.Clear();
+            return path;
+        }
+
+        private void InitStart()
+        {
+            rowQueue.Clear();
+            columQueue.Clear();
+            moveCount = 0;
+            nodesLeftInLayer = 1;
+            nodesInNextLayer = 0;
+            isEnd = false;
+            visitedNodes = new bool[rowSize, columSize];
+            prev = new Node[rowSize, columSize];
+        }
     }
 }
-
-    //    // Driver code
-    //    static void Main(string[] args)
-    //    {
-    //        BfsAlgorithm g = new BfsAlgorithm(4);
-
-    //        g.AddEdge(0, 1);
-    //        g.AddEdge(0, 2);
-    //        g.AddEdge(1, 2);
-    //        g.AddEdge(2, 0);
-    //        g.AddEdge(2, 3);
-    //        g.AddEdge(3, 3);
-
-    //        Console.Write("Following is Breadth First " +
-    //                      "Traversal(starting from " +
-    //                      "vertex 2)\n");
-    //        g.BFS(2);
-    //    }
-    //}
-
-    // This code is contributed by anv89
--- PredmetniZadatak_1/PredmetniZadatak_1/Lines/LineFormer.cs
+++ PredmetniZadatak_1/PredmetniZadatak_1/Lines/LineFormer.cs
@@ -1,4 +1,5 @@
-﻿using PredmetniZadatak_1.Model;
+﻿using PredmetniZadatak_1.BFS;
+using PredmetniZadatak_1.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,8 @@ namespace PredmetniZadatak_1.Lines
         List<DotModel> dotModels = new List<DotModel>();
         Dictionary<long, int> dotModelX = new Dictionary<long, int>();
         Dictionary<long, int> dotModelY = new Dictionary<long, int>();
+        BfsAlgorithm bfs = new BfsAlgorithm();
+        private List<Tuple<double, long, long>> distanceLineIds = new List<Tuple<double, long, long>>();
 
         public LineFormer(List<DotModel> dotModels) : base()
         {
@@ -27,38 +30,91 @@ namespace PredmetniZadatak_1.Lines
 
         public List<Line> AddLine()
         {
+            LeastToMostDistance();
+
             List<Line> lines = new List<Line>();
 
-            for (int i = 0; i < this.LineIDs.Item1.Count; i++)
-            {
-                if(dotModelX.ContainsKey(this.LineIDs.Item1[i]) && dotModelY.ContainsKey(this.LineIDs.Item1[i]) 
-                    && dotModelX.ContainsKey(this.LineIDs.Item2[i]) && dotModelY.ContainsKey(this.LineIDs.Item2[i]))
-                {
-                    lines.Add(new Line()
-                    {
-                        X1 = dotModelX[this.LineIDs.Item1[i]],
-                        Y1 = 960 - dotModelY[this.LineIDs.Item1[i]],
-                        X2 = dotModelX[this.LineIDs.Item2[i]],
-                        Y2 = 960 - dotModelY[this.LineIDs.Item1[i]],
-                        StrokeThickness = 0.5,
-                        Stroke = Brushes.Black,
-                        Fill = Brushes.Black
-                    });
+            //int i = 203;
 
-                    lines.Add(new Line()
+            //if (dotModelX.ContainsKey(this.LineIDs.Item1[i]) && dotModelY.ContainsKey(this.LineIDs.Item1[i])
+            //   && dotModelX.ContainsKey(this.LineIDs.Item2[i]) && dotModelY.ContainsKey(this.LineIDs.Item2[i]))
+            //{
+            //    int startX = dotModelX[this.LineIDs.Item1[i]];
+            //    int startY = 960 - dotModelY[this.LineIDs.Item1[i]];
+            //    int endX = dotModelX[this.LineIDs.Item2[i]];
+            //    int endY = 960 - dotModelY[this.LineIDs.Item2[i]];
+
+            //    int moves = bfs.Solve(startX, startY, endX, endY);
+
+            //    List<Node> nodes = bfs.ReconstructPath(startX, startY, endX, endY);
+
+            //    for (int j = 0; j < nodes.Count - 1; j++)
+            //    {
+            //        lines.Add(new Line()
+            //        {
+            //            X1 = nodes[j].row,
+            //            Y1 = nodes[j].colum,
+            //            X2 = nodes[j + 1].row,
+            //            Y2 = nodes[j + 1].colum,
+            //            StrokeThickness = 0.5,
+            //            Stroke = Brushes.Red,
+            //            Fill = Brushes.Red
+            //        });
+            //    }
+            //}
+
+            for (int i = 0; i < 62; i++)//distanceLineIds.Count
+            {
+                if (dotModelX.ContainsKey(distanceLineIds[i].Item2) && dotModelY.ContainsKey(distanceLineIds[i].Item2)
+               && dotModelX.ContainsKey(distanceLineIds[i].Item3) && dotModelY.ContainsKey(distanceLineIds[i].Item3))
+                {
+                    int startX = dotModelX[distanceLineIds[i].Item2];
+                    int startY = 960 - dotModelY[distanceLineIds[i].Item2];
+                    int endX = dotModelX[distanceLineIds[i].Item3];
+                    int endY = 960 - dotModelY[distanceLineIds[i].Item3];
+
+                    int moves = bfs.Solve(startX, startY, endX, endY);
+
+                    List<Node> nodes = bfs.ReconstructPath(startX, startY, endX, endY);
+
+                    for (int j = 0; j < nodes.Count - 1; j++)
                     {
-                        X1 = dotModelX[this.LineIDs.Item2[i]],
-                        Y1 = 960 - dotModelY[this.LineIDs.Item1[i]],
-                        X2 = dotModelX[this.LineIDs.Item2[i]],
-                        Y2 = 960 - dotModelY[this.LineIDs.Item2[i]],
-                        StrokeThickness = 0.5,
-                        Stroke = Brushes.Black,
-                        Fill = Brushes.Black
-                    });
+                        lines.Add(new Line()
+                        {
+                            X1 = nodes[j].row,
+                            Y1 = nodes[j].colum,
+                            X2 = nodes[j + 1].row,
+                            Y2 = nodes[j + 1].colum,
+                            StrokeThickness = 0.5,
+                            Stroke = Brushes.Red,
+                            Fill = Brushes.Red
+                        });
+                    }
                 }
             }
 
             return lines;
+        }
+
+        private void LeastToMostDistance()
+        {
+            for (int i = 0; i < this.LineIDs.Item1.Count; i++)//this.LineIDs.Item1.Count
+            {
+                if (dotModelX.ContainsKey(this.LineIDs.Item1[i]) && dotModelY.ContainsKey(this.LineIDs.Item1[i])
+               && dotModelX.ContainsKey(this.LineIDs.Item2[i]) && dotModelY.ContainsKey(this.LineIDs.Item2[i]))
+                {
+                    int startX = dotModelX[this.LineIDs.Item1[i]];
+                    int startY = 960 - dotModelY[this.LineIDs.Item1[i]];
+                    int endX = dotModelX[this.LineIDs.Item2[i]];
+                    int endY = 960 - dotModelY[this.LineIDs.Item2[i]];
+
+                    double distance = Math.Sqrt((Math.Pow(startX - endX, 2) + Math.Pow(startY - endY, 2)));
+
+                    distanceLineIds.Add(new Tuple<double, long, long>(distance, this.LineIDs.Item1[i], this.LineIDs.Item2[i]));
+                }
+            }
+
+            distanceLineIds = distanceLineIds.OrderBy(x => x.Item1).ToList();
         }
     }
 }
